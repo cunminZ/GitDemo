0d66e02d625d2cd6ddf5f55a922f3164255b0a5c
zcc <13140433321@163.com>
2019-03-10 18:09:04 +0800

    创建

---
+++ src/main/java/cn/itcast/GitDemo.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+    }
+}
